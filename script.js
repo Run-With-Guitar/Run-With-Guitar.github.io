c102c098e7f5a16185f67133fc83428c9a819b42
Woodern <42751539+Woodern@users.noreply.github.com>
2018-10-08 21:57:41 +0300

    2

--- script.js
+++ script.js
@@ -1,35 +1,47 @@
 $(document).ready(function(){
 
-			// #1
-
 			var character = $('.text_1');
 			var underText = $('.underText_1');
 			var underTextActive = $('.underText_1-active');
+			var sector = $('.sector:nth-child(1)');
+			var sectorActive = $('.sector_1-active');
 			
-			character.hover(function(){
-				underText.toggleClass('underText_1-active')
-				underTextActive.removeClass('underText_1')
-			});
-
-			// #2
-
-			var character2 = $('.text_2');
-			var underText2 = $('.underText_2');
-			var underTextActive2 = $('.underText_2-active');
+			var characterSecond = $('.text_2');
+			var underTextSecond = $('.underText_2');
+			var underTextActiveSecond = $('.underText_2-active');
+			var sectorSecond = $('.sector:nth-child(2)');
+			var sectorActiveSecond = $('.sector_2-active');
 			
-			character2.hover(function(){
-				underText2.toggleClass('underText_2-active')
-				underTextActive2.removeClass('underText_2')
-			});
-
-			// #3
-
 			var character3 = $('.text_3');
 			var underText3 = $('.underText_3');
 			var underTextActive3 = $('.underText_3-active');
+			var sector3 = $('.sector:nth-child(3)');
+			var sectorActive3 = $('.sector_3-active');
+
+			// #1
+			
+			character.hover(function(){
+				underText.toggleClass('underText_1-active')
+				sectorSecond.toggleClass('sector_2-active')
+				sector3.toggleClass('sector_3-active-notSo')
+
+			});
+
+			// #2
+			
+			characterSecond.hover(function(){
+				underTextSecond.toggleClass('underText_2-active')
+				sector1.toggleClass('sector_1-active')
+				sector3.toggleClass('sector_3-active')
+
+			});
+
+			// #3
 			
 			character3.hover(function(){
 				underText3.toggleClass('underText_3-active')
-				underTextActive3.removeClass('underText_3')
+				sector1.toggleClass('sector_1-active')
+				sector3.toggleClass('sector_2-active')
+
 			});
 		});
